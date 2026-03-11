5fbe92cba9c6f636a751b61c616834adcfdde240
nathanxli <nathan.nxli@gmail.com>
2026-03-11 17:50:25 -0400

    Create OrderBook class that tracks bids, ask, and spread

---
+++ src/visualizer/orderbook.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class PriceLevel:
+    price: float
+    size: float
+    num_orders: int
+
+@dataclass
+class OrderBook:
+    product_id:str
+    bids: list[PriceLevel] = field(default_factory=list)
+    asks: list[PriceLevel] = field(default_factory=list)
+    sequence: int | None = None
+
+    def update_from_snapshot(self, snapshot: dict) -> None:
+        self.sequence = snapshot.get("sequence")
+
+        self.bids = [
+            PriceLevel(
+                price = float(level[0]),
+                size = float(level[1]),
+                num_orders=int(level[2])
+            )
+            for level in snapshot.get("bids", [])
+        ]
+
+        self.asks = [
+            PriceLevel(
+                price = float(level[0]),
+                size = float(level[1]),
+                num_orders=int(level[2])
+            )
+            for level in snapshot.get("asks", [])
+        ]
+
+        ## sort snapshots
+        self.bids.sort(key=lambda level: level.price, reverse=True)
+        self.asks.sort(key=lambda level: level.price)
+
+    def best_bid(self) -> PriceLevel | None:
+        return self.bids[0] if self.bids else None
+    
+    def best_ask(self) -> PriceLevel | None:
+        return self.asks[0] if self.asks else None
+    
+    def spread(self) -> float | None:
+        bid = self.best_bid()
+        ask = self.best_ask()
+        if bid is None or ask is None:
+            return None
+        return ask.price - bid.price
+    
+    def summary(self) -> str:
+        bid = self.best_bid()
+        ask = self.best_ask()
+        spread = self.spread()
+
+        if spread is None:
+            return f"{self.product_id}: order book is empty"
+        
+        return (
+            f"{self.product_id} | "
+            f"best bid = {bid.price: .4f} ({bid.size: .9f}) | "
+            f"best ask = {bid.ask: .4f} ({ask.size: .9f}) | "
+            f"spread = {spread: .4f}"
+        )
